9322c7d9c1ca5f108f298531bec8b09a1732d276
Ben Cason <bencason9@gmail.com>
2019-06-06 18:28:22 -0400

    added jawsdb

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,19 @@
 var mysql = require("mysql");
 
 //connection info
-var connection = mysql.createConnection({
+var connection; 
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {connection = mysql.createConnection({
     host: "127.0.0.1",
     port: 3306,
     user: "root",
     password: "root",
     database: "burgers_db"
   });
+}
+    
 
   //connect to db
   connection.connect(function(err) {
@@ -18,3 +24,13 @@ var connection = mysql.createConnection({
   });
 
   module.exports = connection;
+
+  /*
+
+  Host	ou6zjjcqbi307lip.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	
+Username	y57k295skevhpigc	
+Password	o7sr5efnnw2dd9j4	
+Port	3306	
+Database	d42b29g1og7l7h83
+
+*/
